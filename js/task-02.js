f3813fc308c9a3b524501a218536b0591b1d7843
sidorenkoilya <ilyasidorenko1992@gmail.com>
2023-02-21 18:18:52 +0200

    231

---
+++ js/task-02.js
@@ -0,0 +1,12 @@
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const createLI = ingredients.map(el => {
+  const itemEl = document.createElement('li');
+  itemEl.textContent = el;
+  itemEl.classList.add('item');
+  return itemEl;
+});
+
+const getIngrELem = document.querySelector('#ingredients');
+
+getIngrELem.append(...createLI);
---
+++ js/task-09.js
@@ -0,0 +1,14 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const ref = {
+  span: document.querySelector('.color'),
+  button: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+};
+
+ref.button.addEventListener('click', () => {
+  ref.span.textContent = getRandomHexColor();
+  ref.body.style.backgroundColor = ref.span.textContent;
+});
